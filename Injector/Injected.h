7d89c64b19b9c12906e54c901acf495ccb231863
Sam Deane <sam@elegantchaos.com>
2011-12-19 19:27:42 +0000

    added Injected bundle target

---
+++ Injector/Injected.h
@@ -0,0 +1,13 @@
+//
+//  Injected.h
+//  ECInjection
+//
+//  Created by Sam Deane on 19/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Injected : NSObject
+
+@end
---
+++ Injector/Injected.m
@@ -0,0 +1,13 @@
+//
+//  Injected.m
+//  ECInjection
+//
+//  Created by Sam Deane on 19/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Injected.h"
+
+@implementation Injected
+
+@end
