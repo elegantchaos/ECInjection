86ab4693b0a87dd0f2bec75dc175c033e7b9d17b
Sam Deane <sam@elegantchaos.com>
2011-12-20 10:36:41 +0000

    improved injected code

--- Injector/Injected.m
+++ Injector/Injected.m
@@ -3,35 +3,37 @@
 
 @interface Injected()
 
+- (void)installMenu;
 - (IBAction)testAction:(id)sender;
 
 @end
 
 @implementation Injected
 
+static const Injected* gInjectedCode = nil;
+
 + (void)load 
 {
-    NSLog(@"i am in you");
+    NSLog(@"injected code loaded");
+    gInjectedCode = [[Injected alloc] init];
     
+    [gInjectedCode installMenu];
+}
+
+- (void)installMenu
+{
+    NSLog(@"adding Injector menu");
     NSMenu* menu = [[NSApplication sharedApplication] mainMenu];
-    NSLog(@"got menu %@", menu);
-    
     NSInteger index = [menu indexOfItemWithTitle:@"Injector"];
     if (index == -1)
     {
         NSMenuItem* injectorItem = [menu addItemWithTitle:@"Injector" action:nil keyEquivalent:@""];
-        NSLog(@"adding item %@", injectorItem);
-
         NSMenu* injectorMenu = [[NSMenu alloc] initWithTitle:@"Injector"];
-        NSLog(@"adding menu %@", injectorMenu);
-        
         NSMenuItem* testItem = [injectorMenu addItemWithTitle:@"Test" action:@selector(testAction:) keyEquivalent:@""];
         testItem.target = self;
         
         [injectorItem setSubmenu:injectorMenu];
         [injectorMenu release];
-        
-        NSLog(@"menu added");
     }
 }
 
