3ef285cb1499a3b7440434f58d2de4ac83a5a212
Sam Deane <sam@elegantchaos.com>
2011-12-20 10:27:16 +0000

    got injected code building menu

--- Injector/Injected.m
+++ Injector/Injected.m
@@ -1,10 +1,43 @@
 #import "Injected.h"
+#import <AppKit/AppKit.h>
+
+@interface Injected()
+
+- (IBAction)testAction:(id)sender;
+
+@end
 
 @implementation Injected
 
 + (void)load 
 {
     NSLog(@"i am in you");
+    
+    NSMenu* menu = [[NSApplication sharedApplication] mainMenu];
+    NSLog(@"got menu %@", menu);
+    
+    NSInteger index = [menu indexOfItemWithTitle:@"Injector"];
+    if (index == -1)
+    {
+        NSMenuItem* injectorItem = [menu addItemWithTitle:@"Injector" action:nil keyEquivalent:@""];
+        NSLog(@"adding item %@", injectorItem);
+
+        NSMenu* injectorMenu = [[NSMenu alloc] initWithTitle:@"Injector"];
+        NSLog(@"adding menu %@", injectorMenu);
+        
+        NSMenuItem* testItem = [injectorMenu addItemWithTitle:@"Test" action:@selector(testAction:) keyEquivalent:@""];
+        testItem.target = self;
+        
+        [injectorItem setSubmenu:injectorMenu];
+        [injectorMenu release];
+        
+        NSLog(@"menu added");
+    }
+}
+
+- (IBAction)testAction:(id)sender
+{
+    NSLog(@"test action fired");
 }
 
 @end
